6cb6891ef77f817688d493b3f3f7eb95e6e5fa36
Strupp1 <65494785+Strupp1@users.noreply.github.com>
2024-09-06 20:21:10 +0200

    make files chrome-ready

--- src/content.js
+++ src/content.js
@@ -1,6 +1,9 @@
 window.addEventListener("pageshow", function () {
 	console.log("BC Stock Count Extension Loaded!");
 	var s = document.createElement('script');
+	if (typeof browser === "undefined") {
+		var browser = chrome;
+	}
 	s.src = browser.runtime.getURL('src/script.js');
 	s.onload = function () {
 		this.remove();
